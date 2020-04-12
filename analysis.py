0d2043ef050a039784c8a561b6337a70e2138b36
JackCaff <JackCaff@users.noreply.github.com>
2020-04-12 19:31:54 +0100

    Inital Commit for Writing to Text file, & saving Histogram as a PNG File.

--- analysis.py
+++ analysis.py
@@ -6,13 +6,28 @@
 import pandas as  pd
 import matplotlib.pyplot as plt
 
+from matplotlib import pyplot as plt #Imports the library to save output as PNG file. 
+
 df = pd.read_csv("iris_csv.csv")
-f = open("Summary.txt","a") #created Summary.txt file and appends to this file
-print (df) # used to display Summary. 
+
+
+x=df["sepallength"].max()
+y=df["sepallength"].min()
+z=df["sepallength"].count()
+
+
+f = open("Summary.txt","w") #created Summary.txt file and appends to this file
+f.write(f"The total number of samples recorded is {z}. \nThese samples are a combination of 3 sets of 50 Iris flower classes. These classes include: \nSetosa \nVersicolor \nVirginica. \n") # used to display Summary.
+f.write(f" The largest value recorded for Sepal Length was {x} the smallest value recorded was {y} from the {z} samples.\n")
 f.close() #close Summary.txt
 
 plt.scatter(df['sepallength'], df['sepalwidth']) #plots two variables to a scatter plot
 
-df.hist(bins=150) #plots a histogram of the various variables for the CSV file
+plt.show() #Displays Scatter Plot
 
-plt.show()# displays both plots.
+
+sepallenght = (df["sepallength"])
+plt.hist([sepallenght]) #plts the Histogram 
+plt.savefig('Sepal_Length.png')
+plt.show()# displays Histograms .
+
